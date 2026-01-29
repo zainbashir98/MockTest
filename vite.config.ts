34e2a27d6b48a9a71dc7f5273d59a367d23265ed
zainbashir98 <125275586+zainbashir98@users.noreply.github.com>
2026-01-29 18:05:59 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
-
+import 
 export default defineConfig({
+base: '/MockTest/',
+  
   plugins: [
     react(),
     runtimeErrorOverlay(),
